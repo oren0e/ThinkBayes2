89a219c8e459c6992f39e4cd9715c132b2951a33
Oren Epshtain <oren@gojuno.com>
2019-03-15 21:29:05 +0200

    added code for Cookie problem

--- thinkbayes2/chap2.py
+++ thinkbayes2/chap2.py
@@ -11,3 +11,58 @@ for word in word_list:
 pmf.Normalize()
 print(pmf.Prob('this'))
 
+# The cookie problem
+pmf = Pmf()
+pmf.Set('Bowl 1', 0.5)
+pmf.Set('Bowl 2', 0.5)
+
+pmf.Mult('Bowl 1', 0.75)
+pmf.Mult('Bowl 2', 0.5)
+pmf.Normalize()
+print(pmf.Prob('Bowl 1'))
+
+class Cookie(Pmf):
+
+    def __init__(self, hypos):
+        Pmf.__init__(self, hypos)
+        for hypo in hypos:
+            self.Set(hypo, 1)
+        self.Normalize()
+
+    def Update(self, data):
+        for hypo in self.Values():
+            like = self.Likelihood(data, hypo)
+            self.Mult(hypo, like)
+        self.Normalize()
+
+    mixes = {
+        'Bowl 1': dict(vanilla=0.75, chocolate=0.25),
+        'Bowl 2': dict(vanilla=0.5, chocolate=0.5),
+    }
+
+    def Likelihood(self, data, hypo):
+        mix = self.mixes[hypo]
+        like = mix[data]
+        return like
+
+pmf = Cookie(['Bowl 1','Bowl 2'])
+pmf.Update('vanilla')
+pmf.Print()
+
+for hypo, prob in pmf.Items():
+    print(hypo, prob)
+
+dataset = ['vanilla','chocolate', 'vanilla']
+for data in dataset:
+    pmf.Update(data)
+
+
+# Monty Hall Problem #
+
+
+
+
+
+
+
+
