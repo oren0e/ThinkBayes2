8aa5ee2df67a635dd3373a05d8e5760d4d10fe81
Oren Epshtain <oren@gojuno.com>
2019-04-02 08:17:05 +0300

    added coding for the monty hall problem.

--- thinkbayes2/chap2.py
+++ thinkbayes2/chap2.py
@@ -58,7 +58,36 @@ for data in dataset:
 
 
 # Monty Hall Problem #
+class Monty(Pmf):
+    def __init__(self, hypos):
+        Pmf.__init__(self)
+        for hypo in hypos:
+            self.Set(hypo, 1)
+        self.Normalize()
 
+    def Update(self, data):
+        for hypo in self.Values():
+            like = self.Likelihood(data, hypo)
+            self.Mult(hypo, like)
+        self.Normalize()
+
+    def Likelihood(self, data, hypo):
+        if hypo == data:
+            return 0
+        elif hypo == 'A':
+            return 0.5
+        else:
+            return 1
+
+
+hypos = 'ABC'
+pmf = Monty(hypos)
+data = 'B'  # Monty opens door B
+pmf.Update(data)
+
+# Print the result
+for hypo, prob in pmf.Items():
+    print(hypo, prob)
 
 
 
