4409ce8e3ce45cb8d628466d6e2574970a038cff
Oren Epshtain <oren@gojuno.com>
2019-04-02 09:20:08 +0300

    Finished chapter 2.

--- thinkbayes2/chap2.py
+++ thinkbayes2/chap2.py
@@ -1,6 +1,6 @@
 from thinkbayes2.thinkbayes2 import Pmf
 
-pmf = Pmf()
+pmf = Pmf('ab')
 for x in [1,2,3,4,5,6]:
     pmf.Set(x, 1/6.0)
 
@@ -89,9 +89,67 @@ pmf.Update(data)
 for hypo, prob in pmf.Items():
     print(hypo, prob)
 
+# Because several parts are the same we can encapsulate them in an object:
+class Suite(Pmf):
+    """Represents a suite of hypotheses and their probabilities."""
+
+    def __init__(self, hypos = tuple()):
+        """Initializes the distribution."""
+        Pmf.__init__(self)
+        for hypo in hypos:
+            self.Set(hypo, 1)
+        self.Normalize()
+
+    def Update(self, data):
+        """Updates each hypothesis based on the data."""
+        for hypo in self.Values():
+            like = self.Likelihood(data, hypo)
+            self.Mult(hypo, like)
+        self.Normalize()
+
+    def Print(self):
+        """Prints the hypotheses and their probabilities."""
+        for hypo, prob in sorted(self.Items()):
+            print(hypo, prob)
+
+# To use Suite we should write a class that inherits from it and provides Likelihood method.
+# Here is a solution for the Monty-Hall problem:
+
+from thinkbayes2.thinkbayes2 import Suite
 
 
+class Monty(Suite):
+    def Likelihood(self, data, hypo):
+        if hypo == data:
+            return 0
+        elif hypo == 'A':
+            return 0.5
+        else:
+            return 1
 
 
+suite = Monty('ABC')
+suite.Update('B')
+suite.Print()
+
+# The M&M problem
+class M_and_M(Suite):
+    mix94 = dict(brown=30, yellow=20, red=20, green=10, orange=10, tan=10)
+    mix96 = dict(blue=24, green=20, orange=16, yellow=14, red=13, brown=13)
+
+    hypoA = dict(bag1=mix94, bag2=mix96)
+    hypoB = dict(bag1=mix96, bag2=mix94)
+
+    hypotheses = dict(A=hypoA, B=hypoB)
+
+    def Likelihood(self, data, hypo):
+        bag, color = data
+        mix = self.hypotheses[hypo][bag]
+        like = mix[color]
+        return like
 
 
+suite = M_and_M('AB')
+suite.Update(('bag1', 'yellow'))
+suite.Update(('bag2', 'green'))
+suite.Print()
