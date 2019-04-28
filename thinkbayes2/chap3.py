579b3d98045a2657cebc888ace17cc1715f11331
Oren Epshtain <oren@gojuno.com>
2019-04-28 08:09:43 +0300

    cont chap3.

--- thinkbayes2/chap3.py
+++ thinkbayes2/chap3.py
@@ -3,6 +3,7 @@ from thinkbayes2.thinkbayes2 import Suite
 # The Dice Problem #
 suite = Dice([4, 6, 8, 12, 20])
 
+
 class Dice(Suite):
     def Likelihood(self, data, hypo):
         if hypo < data:
@@ -12,3 +13,24 @@ class Dice(Suite):
 
 
 suite.Update(6)
+suite.Print()
+
+for roll in [6, 8, 7, 7, 5, 4]:
+    suite.Update(roll)
+suite.Print()
+
+# The locomotive problem
+hypos = range(1, 1001)
+
+
+class Train(Suite):
+    def Likelihood(self, data, hypo):
+        if hypo < data:
+            return 0
+        else:
+            return 1.0/hypo
+
+
+suite = Train(hypos)
+suite.Update(60)
+print(suite.Mean())
