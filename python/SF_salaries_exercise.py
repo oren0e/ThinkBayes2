0c98fe99c1d8dafb562df99b39becc7ff3591c21
Oren Epshtain <oren@gojuno.com>
2019-11-04 21:03:49 +0200

    SF salaries with answers.

--- python/SF_salaries_exercise.py
+++ python/SF_salaries_exercise.py
@@ -17,31 +17,50 @@ sal["OvertimePay"].max()
 # What is the job title of JOSEPH DRISCOLL?
 # Note: Use all caps, otherwise you may get an answer that doesn't match up (there is also a lowercase Joseph Driscoll)
 sal.loc[sal.EmployeeName == 'JOSEPH DRISCOLL','JobTitle']
+sal[sal['EmployeeName'] == 'JOSEPH DRISCOLL']['JobTitle']  # answer
 
 # How much does JOSEPH DRISCOLL make (including benefits)?
 sal.loc[sal.EmployeeName == 'JOSEPH DRISCOLL','TotalPayBenefits']
+sal[sal['EmployeeName'] == 'JOSEPH DRISCOLL']['TotalPayBenefits']  # answer
+
 
 # What is the name of highest paid person (including benefits)?
 sal.loc[sal['TotalPayBenefits'].idxmax()]
+sal[sal['TotalPayBenefits'] == sal['TotalPayBenefits'].max()]['EmployeeName']  # answer
+
 
 # What is the name of lowest paid person (including benefits)?
 # Do you notice something strange about how much he or she is paid?
 sal.loc[sal['TotalPayBenefits'].idxmin()]
+sal.iloc[sal['TotalPayBenefits'].argmin()]  # answer
 
 # What was the average (mean) BasePay of all employees per year? (2011-2014)?
 sal.groupby('Year')['BasePay'].mean()
 
 # How many unique job titles are there?
 sal['JobTitle'].nunique()
+len(sal['JobTitle'].unique())
 
 # What are the top 5 most common jobs?
 sal.groupby('JobTitle')['JobTitle'].count().nlargest(5)
+sal['JobTitle'].value_counts().head(5)  # answer
 
 # How many Job Titles were represented by only one person in 2013? (e.g. Job Titles with only one occurence in 2013?)
 sum(sal[sal.Year == 2013].groupby(['JobTitle'])['JobTitle'].count() == 1)
+sum(sal[sal['Year'] == 2013]['JobTitle'].value_counts() == 1)
+
 
 # How many people have the word Chief in their job title? (This is pretty tricky)
 sum(sal['JobTitle'].str.contains(".*Chief.*", regex = True))
+
+
+def cheif_string(title):
+    if 'chief' in title.lower().split():
+        return True
+    else:
+        return False
+
+sum(sal['JobTitle'].apply(lambda x: cheif_string(x)))
 
 # Bonus: Is there a correlation between length of the Job Title string and Salary?
 sal['JobTitle_len'] = sal['JobTitle'].apply(len)
